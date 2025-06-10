ff9c7d70fbb0f3c153f0ac92e0cdcda464441099
Neven Dimač <neven.dimac@gmail.com>
2025-06-10 11:54:16 +0200

    Ussing additional styles

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,6 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list_item.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
   const ExpensesList({
@@ -17,6 +17,11 @@ class ExpensesList extends StatelessWidget {
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withValues(alpha: 0.25),
+          alignment: Alignment.centerRight,
+          padding: const EdgeInsets.only(right: 20),
+        ),
         key: ValueKey(expenses[index]),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
